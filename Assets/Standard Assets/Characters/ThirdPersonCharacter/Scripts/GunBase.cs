e023dcbee87fcba655685405a437706a5e0b9437
hews0014 <39865988+hews0014@users.noreply.github.com>
2019-02-19 14:01:57 -0500

    Added gun base class, pistol script, and plasma rifle script

---
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/GunBase.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunBase : MonoBehaviour
+{
+    public enum GunType
+    {
+        Pistol,
+        PlasmaRifle,
+        None
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        m_FireDelay = 0.0f;
+        m_GunType = GunType.None;
+    }
+	
+	// Update is called once per frame
+	virtual protected void Update ()
+    {
+        if (m_CanFire == false)
+        {
+            m_FireDelayTimer -= Time.fixedDeltaTime;
+
+            if (m_FireDelayTimer <= 0.0f)
+            {
+                m_CanFire = true;
+                m_FireDelayTimer = m_FireDelay;
+            }
+        }
+    }
+
+    public float m_FireDelay;
+    public GunType m_GunType;
+
+    public bool m_CanFire;
+    public float m_FireDelayTimer;
+}
---
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/PlasmaRifle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlasmaRifle : GunBase
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_GunType = GunType.Pistol;
+        m_FireDelay = 0.5f;
+        m_FireDelayTimer = m_FireDelay;
+
+        m_CanFire = true;
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    public void Fire()
+    {
+        if (m_CanFire == true)
+        {
+            m_CanFire = false;
+        }
+    }
+}
