7016f2f6176154c16e2600d644cce225685b1273
hews0014 <39865988+hews0014@users.noreply.github.com>
2019-02-19 20:44:47 -0500

    Finished spawner functionality

---
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/Enemy/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public float SpawnDelay;
+    public Enemy SpawningEnemy;
+    public Vector3 SpawnOffset;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_SpawnTimer = SpawnDelay;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        m_SpawnTimer -= Time.fixedDeltaTime;
+
+        SpawnEnemy();
+    }
+
+    void SpawnEnemy()
+    {
+        if (m_SpawnTimer <= 0.0f)
+        {
+            m_SpawnTimer = SpawnDelay;
+
+            Vector3 pos = transform.position;
+            pos.y += 2.0f;
+            Instantiate(SpawningEnemy, pos, transform.rotation);
+        }
+    }
+    
+    float m_SpawnTimer;
+}
